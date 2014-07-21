dec0d6f4222fbba67fcb33f852e71f47c74520f3
Bean <188513331@qq.com>
2014-07-21 21:48:19 +0800

    项目更新

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://127.0.0.1:8080/cban/',
+    'authkey' => 'iQBx8suWiNrEuqpn1U6Mn1CL8kIc9ZwN',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
