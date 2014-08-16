e79a31b73cd968254f51142dd1dee9d479594228
Bean <188513331@qq.com>
2014-08-16 19:59:04 +0800

    GD2测试

--- phpcms/modules/cban/index.php
+++ phpcms/modules/cban/index.php
@@ -357,5 +357,24 @@ class index {
 			include template('content','jdwx_show');
 		}
 	}
+	
+	function aaa(){
+		
+		$image = imagecreatetruecolor(200,100);
+		
+		//设置背景颜色
+		$bgcolor = imagecolorallocate($image,0,0,0);
+		
+		//设置字体颜色
+		$textcolor = imagecolorallocate($image,255,255,255);
+		
+		//把字符串写在图像左上角
+		imagestring($image,20,15,10,"Hello world!",$textcolor);
+		
+		//输出图像
+		header("Content-type: image/jpeg");
+		imagejpeg($image);
+		
+	}
 }
 ?>
